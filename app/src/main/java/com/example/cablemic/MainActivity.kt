1528e95bafdd66ebc25b7d8a39f6da4d82c53339
Natsu-Dev <Natsu-aoiro@s.okayama-u.ac.jp>
2020-12-26 16:44:30 +0900

    Runtime Permission対応

--- app/src/main/java/com/example/cablemic/MainActivity.kt
+++ app/src/main/java/com/example/cablemic/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cablemic
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,6 +16,8 @@ import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSIONS_REQUEST_CODE = 1
+
     private val smplRate = 44100 // Hz
     private val frRate = 10 // fps，毎秒の処理回数
     private val shortPerFrame = smplRate / frRate // フレーム当たりの音声データ数
@@ -22,16 +27,17 @@ class MainActivity : AppCompatActivity() {
             AudioFormat.CHANNEL_IN_MONO,
             AudioFormat.ENCODING_PCM_16BIT))
 
-    private fun startRecording() {
-        val audioRecord = AudioRecord( // インスタンス召喚の儀
-            MediaRecorder.AudioSource.MIC, // 入力音源はマイク
-            smplRate, // 44100Hz
-            AudioFormat.CHANNEL_IN_MONO, // モノトラック
-            AudioFormat.ENCODING_PCM_16BIT, // PCM 16bit
-            bufSize) // バッファ
+    private val audioRecord = AudioRecord( // インスタンス召喚の儀
+        MediaRecorder.AudioSource.MIC, // 入力音源はマイク
+        smplRate, // 44100Hz
+        AudioFormat.CHANNEL_IN_MONO, // モノトラック
+        AudioFormat.ENCODING_PCM_16BIT, // PCM 16bit
+        bufSize) // バッファ
+
+    private fun startRec() {
 
         audioRecord.positionNotificationPeriod = shortPerFrame // フレーム当たりの処理数を指定
-        audioRecord.notificationMarkerPosition = 10000 // マーカー周期を指定
+        audioRecord.notificationMarkerPosition = 50000 // マーカー周期を指定
         val audioArray = ShortArray(shortPerFrame) // 音声データを格納する配列
 
         audioRecord.setRecordPositionUpdateListener(object :
@@ -54,16 +60,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun buttonClick(v: View) { // ボタンが押された時の処理
         val aButton = v as ImageButton // 画像ボタンを指定
-        active = !active // ON/OFF反転
 
-        if (active) // マイクONのとき
-        {
+        if (!active) { // マイクがOFFのとき
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // Android 6.0以降について
+                if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) { // マイクの使用が許可されていない場合
+                    requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO),
+                        PERMISSIONS_REQUEST_CODE) // リクエストを送信
+                }
+                    if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) { // 許可されなかった場合
+                        return // とりあえず終了
+                }
+            }
+
             aButton.setImageResource(R.drawable._0) // マイクONの画像に変更
-            startRecording() // マイクON
-        } else {
+            startRec() // マイクON
+            active = true // ONにする
+
+        } else { // マイクをOFFにしたとき
             aButton.setImageResource(R.drawable.`_`) // マイクOFFの画像に変更
             Log.v("AudioRecord", "stop")
-            //audioRecord.stop() // マイクOFF
+            audioRecord.stop() // マイクOFF
+            active = false // OFFにする
         }
     }
 
@@ -72,9 +90,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageButton: ImageButton = findViewById(R.id.imageButton)
-        imageButton.setOnClickListener {
-            buttonClick(imageButton)
+            val imageButton: ImageButton = findViewById(R.id.imageButton)
+            imageButton.setOnClickListener {
+                buttonClick(imageButton)
+            }
         }
     }
-}
